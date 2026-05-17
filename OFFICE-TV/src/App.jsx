8053d83f666aa59470b0e53918fec79c3375436b
themysterysolver <arjunprabhakar1910@gmail.com>
2026-05-17 17:13:34 +0530

    FEAT:colors and speed

--- OFFICE-TV/src/App.jsx
+++ OFFICE-TV/src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import Headerr from './components/Headerr'
 import Tv from './components/Tv'
 import './App.css'
+import Speed from './components/Speed'
+// import { useState } from 'react'
 
 function App() {
   
+  let [speed,setSpeed] = useState(100);
 
   return (
     <>
       <Headerr/>
-      <Tv/>
+      <Speed speed={speed} setSpeed={setSpeed}/>
+      <Tv speed={speed}/>
     </>
   )
 }
--- OFFICE-TV/src/components/Tv.jsx
+++ OFFICE-TV/src/components/Tv.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from 'react'
 import { useEffect,useRef } from 'react'
 
-const Tv = () => {
+const Tv = ({speed}) => {
     let DVD = useRef(null)
     let [coordinate,setCoordinate] = useState([0,0]) 
     let TV = useRef(null);
+    
+    let dirL = useRef(10)
+    let dirT = useRef(10)
+
     useEffect(()=>{
         const el = DVD.current;
         const tvEl = TV.current;
@@ -24,8 +28,8 @@ const Tv = () => {
         
         let idx = 0;
         // let dir = [[10,10],[-10,10],[-10,-10],[10,-10]]
-        let dirL = 12
-        let dirT = 12
+        // let dirL = 1
+        // let dirT = 1
         
         tvEl.style.position = 'relative'
         el.style.position = 'absolute'
@@ -35,35 +39,41 @@ const Tv = () => {
         let i = setInterval(()=>{
             setCoordinate(([x,y])=>{
                 
-                let left = x+dirL;
-                let top = y+dirT;
-                if(left<0 || left>tvEl.clientWidth-el.offsetWidth){
-                  dirL*=-1
-                  left = x+dirL;
-                }
-                if(top<0 || top>tvEl.clientHeight-el.offsetHeight){
-                  dirT*=-1
-                  top = y+dirT;
-                }
+                let left = x+dirL.current*1/speed;
+                let top = y+dirT.current*1/speed;
                 if((left <= 0 && top <= 0)||(left<=0 && top >= tvEl.clientHeight-el.offsetHeight) 
                   || (left >= tvEl.clientWidth-el.offsetWidth && top <= 0) 
                   || (left >= tvEl.clientWidth-el.offsetWidth && top>=tvEl.clientHeight-el.offsetHeight)){
+                    el.style.left = `${left}px`;
+                    el.style.top = `${top}px`;
                     alert('yeahh!')  
+                    location.reload()
                   }
+                if(left<0 || left>tvEl.clientWidth-el.offsetWidth){
+                  dirL.current*=-1
+                  left = x+dirL;
+                }
+                if(top<0 || top>tvEl.clientHeight-el.offsetHeight){
+                  dirT.current*=-1
+                  top = y+dirT;
+                }
+                
                 
                 el.style.left = `${left}px`;
                 el.style.top = `${top}px`;
+                // console.log(speed)
                 return [left,top];
             });
-        },1);
+        },16);
+        console.log(speed)
         return ()=> clearInterval(i)
-        console.log('hello:',el.style.left,el.style.top)
-    },[])
+    },[speed])
     
   return (
     <div className='TV' ref={TV}>
       <div ref = {DVD} className='DVD' >
-        DVD
+        <span style={{color:"black"}}>DVD</span>
+        <span style={{color:"white"}}>VIDEOS</span>
       </div>
     </div>
   )
